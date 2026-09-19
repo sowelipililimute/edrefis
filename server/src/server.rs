db3f0c4a523cacdc3e6aed2e8615f4753c1a148c
Janet Blackquill <uhhadd@gmail.com>
2026-09-18 22:43:24 -0400

    oopsy daisy

--- server/src/server.rs
+++ server/src/server.rs
@@ -11,17 +11,17 @@ impl Endpoint for ServerEndpoint {
     type Outgoing = (u32, ServerToClient);
     type State = ();
 
-    fn handle_packet(peer: &mut Peer<Self>, (_client_id, packet): Self::Incoming) {
+    fn handle_packet(peer: &mut Peer<Self>, (client_id, packet): Self::Incoming) {
         match packet {
             ClientToServer::Join => {
                 spawn_field(&mut peer.world);
                 let states = peer.registry.serialize_world(&mut peer.world);
                 peer.outgoing_packets
-                    .push_back((0, ServerToClient::States(states)));
+                    .push_back((client_id, ServerToClient::States(states)));
             }
             ClientToServer::Ping(secs, nanos) => {
                 peer.outgoing_packets
-                    .push_back((0, ServerToClient::Pong(secs, nanos)));
+                    .push_back((client_id, ServerToClient::Pong(secs, nanos)));
             }
         }
     }
